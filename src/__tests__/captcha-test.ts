f30673a2053d27fd603f16b5d90c81619a8e8de2
Kanokgan Hanmoungtham <kanokgan@gmail.com>
2016-09-10 14:37:24 +0700

    Added lite-server for simple test on browser

--- src/__tests__/captcha-test.ts
+++ src/__tests__/captcha-test.ts
@@ -1,41 +1,6 @@
 /// <reference path="../typings/jest/jest.d.ts"/>
 
-class Captcha {
-	generate(pattern: number, left: number, right: number, operator: number): string {
-		var output = "";
-		var numberString = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
-		var operatorString = ['+', '-', '*'];
-
-		if (pattern == 1) 
-		{
-			output += left;
-
-			output += ' ';
-
-			output += operatorString[operator - 1];
-
-			output += ' ';
-
-			output += numberString[right - 1];
-
-		} else if (pattern == 2)
-		{
-			output += numberString[left -1];
-
-			output += ' ';
-
-			output += operatorString[operator - 1];
-
-			output += ' ';
-
-			output += right;
-
-		}
-
-		return output;
-	};
-}
-
+import {Captcha} from "../js/captcha"
 
 describe('Captcha', function() {
 	describe('Pattern 1', function() {
--- src/js/captcha.js
+++ src/js/captcha.js
@@ -1,10 +1,28 @@
-function Captcha() {
-
-}
-
-Captcha.prototype.generate = function(pattern, left, right, operator) {
-	if (right == 2)
-		return "1 + Two";	
-	if (right == 3)
-		return "1 + Three";
-};
+"use strict";
+var Captcha = (function () {
+    function Captcha() {
+    }
+    Captcha.prototype.generate = function (pattern, left, right, operator) {
+        var output = "";
+        var numberString = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'];
+        var operatorString = ['+', '-', '*'];
+        if (pattern == 1) {
+            output += left;
+            output += ' ';
+            output += operatorString[operator - 1];
+            output += ' ';
+            output += numberString[right - 1];
+        }
+        else if (pattern == 2) {
+            output += numberString[left - 1];
+            output += ' ';
+            output += operatorString[operator - 1];
+            output += ' ';
+            output += right;
+        }
+        return output;
+    };
+    ;
+    return Captcha;
+}());
+exports.Captcha = Captcha;
